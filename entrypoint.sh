1e67df2292263be36eff5a09f52345a8b5b0f272
Owen Barton <owen@owenbarton.com>
2021-02-15 09:54:45 -0800

    Limit files scanned to avoid hitting 65535 character Github API limit.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,13 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+# Only scan added and modified files to reduce volume of output
+# - avoids hitting 65535 character Github API limit.
+export FILES=$(git diff --diff-filter=AM --name-status master | cut -f2)
+
 # NOTE: ${VAR,,} Is bash 4.0 syntax to make strings lowercase.
 echo "[action-remark-lint] Checking markdown code with the remark-lint linter and reviewdog..."
-remark --rc-path=/src/remarkrc.suggestion . ${INPUT_REMARK_ARGS} 2>&1 |
+remark --rc-path=/src/remarkrc.suggestion ${FILES} ${INPUT_REMARK_ARGS} 2>&1 |
   sed 's/\x1b\[[0-9;]*m//g' | # Removes ansi codes see https://github.com/reviewdog/errorformat/issues/51
   reviewdog -f=remark-lint \
     -name="${INPUT_TOOL_NAME}" \
@@ -20,7 +24,7 @@ remark --rc-path=/src/remarkrc.suggestion . ${INPUT_REMARK_ARGS} 2>&1 |
     -tee \
     ${INPUT_REVIEWDOG_FLAGS}
 
-remark --rc-path=/src/remarkrc.problem . ${INPUT_REMARK_ARGS} 2>&1 |
+remark --rc-path=/src/remarkrc.problem ${FILES}: ${INPUT_REMARK_ARGS} 2>&1 |
   sed 's/\x1b\[[0-9;]*m//g' | # Removes ansi codes see https://github.com/reviewdog/errorformat/issues/51
   reviewdog -f=remark-lint \
     -name="${INPUT_TOOL_NAME}" \
